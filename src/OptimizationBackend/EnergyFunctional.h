1a719add6fd3cba18c7952afe126c1a673473b0d
blindnote <cherrypisces021@gmail.com>
2017-11-21 16:18:51 -0800

    correct some eigen bugs

--- src/OptimizationBackend/EnergyFunctional.h
+++ src/OptimizationBackend/EnergyFunctional.h
@@ -119,6 +119,7 @@ public:
 	  std::less<uint64_t>,
 	  Eigen::aligned_allocator<std::pair<uint64_t, Eigen::Vector2i>>
 	  > connectivityMap;
+//	std::map<uint64_t,Eigen::Vector2i> connectivityMap;
 
 private:
 
