2f6c29e45160fc5de914203a51309b670f53c628
blindnote <cherrypisces021@gmail.com>
2017-12-15 12:38:25 -0800

    fix eigen bugs

--- src/OptimizationBackend/EnergyFunctional.h
+++ src/OptimizationBackend/EnergyFunctional.h
@@ -114,12 +114,12 @@ public:
 	IndexThreadReduce<Vec10>* red;
 
 
-	std::map<uint64_t,
-	  Eigen::Vector2i,
-	  std::less<uint64_t>,
-	  Eigen::aligned_allocator<std::pair<uint64_t, Eigen::Vector2i>>
-	  > connectivityMap;
-//	std::map<uint64_t,Eigen::Vector2i> connectivityMap;
+//	std::map<uint64_t,
+//	  Eigen::Vector2i,
+//	  std::less<uint64_t>,
+//	  Eigen::aligned_allocator<std::pair<uint64_t, Eigen::Vector2i>>
+//	  > connectivityMap;
+	std::map<uint64_t,Eigen::Vector2i> connectivityMap;
 
 private:
 
